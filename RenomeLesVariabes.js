80f4d5ed5cfaca566fe6274791ccb881225535fb
Rémi LABONNE <remilabonne@yahoo.fr>
2024-05-13 11:54:37 +0200

    Ajout de fichier

---
+++ RenomeLesVariabes.js
@@ -0,0 +1,28 @@
+
+  /*
+    🐦 DAVID Bruno Twitter profile 🐦
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+    const name = "David"; // ✅ Good!  
+    const lastName = "Bruno"; // ❌ the keyword to declare the variable is missing 
+  
+// à partir d'ici j'utilise la Upper Camel Case
+
+    let Biography = "Hi, I'm David Bruno from SF, I like to cook and meet new people."; // ❌ the variable name is not explicit. (this is David's biography)
+  
+  
+    const LinkProfilPicture = "http://www.go.com/davif.png"; // ❌ the variable name is not explicit and the keyword is missing (this is David's profile picture)
+  
+    let Address = "San Francisco"; // ❌ the variable name is not explicit and the keyword is missing (try to guess what "San Francisco" could refer to)
+  
+    let Followers = 109; // ❌ the keyword to declare the variable is missing 
+    var Following = 200; // ❌ the keyword to declare the variable is missing 
+  
+  // 🏁 Exercise 02 - David is following one more account increment the total of following account
+
+Following++;
