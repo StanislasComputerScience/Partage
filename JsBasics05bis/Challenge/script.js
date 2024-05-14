a9ec1acf5b3644076fac1dff95e83a61c2f35167
Rémi LABONNE <remilabonne@yahoo.fr>
2024-05-14 07:10:59 +0200

    modification du programme challege

--- JsBasics05bis/Challenge/script.js
+++ JsBasics05bis/Challenge/script.js
@@ -1,27 +1,18 @@
-// Declare and define three variables using a numerical value
-
-// * If the three variables are equal, display "The three variables are the same" in the console
-// * If only two of the variables are equal, display "Two of the variables are the same" in the console
-// * If the variables are all different display "all variables are different" in the console
-
-let a = prompt("Entrez un nombre"); // 5
-let b = prompt("Entrez un nombre"); // 5
-let c = prompt("Entrez un nombre"); // 2
+let a = prompt("Entrez un nombre");
+let b = prompt("Entrez un nombre");
+let c = prompt("Entrez un nombre");
 
 let str;
 
-if (a == b) {
-  if (b == c) {
-    str = "The three variables are the same";
-  } else if (b == c) {
-    str = "two variables are the same";
-  } else {
-    str = "All variables are different";
-  }
-} else {
+if (a == b && b == c) {
+  str = "The three variables are the same";
+} else if (a == b || a == c || b == c) {
   str = "Two of the variables are the same";
+} else {
+  str = "All variables are differents";
 }
-console.log(A);
+
+console.log(a);
 
 // Mettre à jour le contenu des éléments HTML
 document.getElementById("a").textContent = a;
